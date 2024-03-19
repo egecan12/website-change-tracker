a4090cf5e0d1ee9ceac0685166a42fc29ded7c1f
egecan12 <egecan18k@gmail.com>
2024-03-19 05:11:18 -0400

    Update example.env and emailTemplates.js

--- utils/emailSender/emailTemplates.js
+++ utils/emailSender/emailTemplates.js
@@ -1,9 +1,14 @@
 const { sendEmail } = require("./emailSender");
 
 exports.sendContentChangedEmail = async (to, url) => {
+  const spreadsheetId = process.env.SPREADSHEET_ID;
+
+  // Generate the Google Sheets URL
+  const googleSheetsUrl = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/edit#gid=0`;
+  // Generate the Email
   const subject = `Content Changed in ${url}`;
-  const text = `Hi there, the content you are tracking has changed.`;
-  const html = `<p>Hello! Just a friendly reminder that there have been some updates to the content you're keeping an eye on. You can check out the changes at ${url}.</p>`;
+  const text = `Hi there, the content you are tracking on ${url}, has changed !`;
+  const html = `<p>Hello! Just a friendly reminder that there have been some updates on ${url}. You can check out the changes at ${googleSheetsUrl}.</p>`;
 
   await sendEmail(to, subject, text, html);
 };
