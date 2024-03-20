3e4ef06788d47baad5473fe157f45b3006c6293b
egecan12 <egecan18k@gmail.com>
2024-03-20 08:36:31 -0400

    Update cron job schedule

--- cronJobs.js
+++ cronJobs.js
@@ -2,9 +2,9 @@ const cron = require("node-cron");
 const { start } = require("./controllers/appLogicController"); // replace with actual file and function
 
 exports.job1 = cron.schedule(
-  process.env.CRON_TIMER,
+  process.env.CRON_TIMER || "*/5 * * * *",
   () => {
-    console.log("Running job1 every minute");
+    console.log("Running job1");
     start();
   },
   {
