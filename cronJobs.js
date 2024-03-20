1847dcf57fd71e6b0bb04afceb415582715e25f5
egecan12 <egecan18k@gmail.com>
2024-03-20 09:00:59 -0400

    Refactor cron job to check for CRON_TIMER environment variable

--- cronJobs.js
+++ cronJobs.js
@@ -1,13 +1,17 @@
 const cron = require("node-cron");
 const { start } = require("./controllers/appLogicController"); // replace with actual file and function
 
-exports.job1 = cron.schedule(
-  process.env.CRON_TIMER || "*/5 * * * *",
-  () => {
-    console.log("Running job1");
-    start();
-  },
-  {
-    scheduled: false,
-  }
-);
+if (process.env.CRON_TIMER) {
+  exports.job1 = cron.schedule(
+    process.env.CRON_TIMER,
+    () => {
+      console.log("Running job1");
+      start();
+    },
+    {
+      scheduled: false,
+    }
+  );
+} else {
+  console.log("CRON_TIMER is not set. The job will not run.");
+}
